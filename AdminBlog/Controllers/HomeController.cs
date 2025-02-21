0cf9d732c5e4967f1e517b5a783cd79e87bec55d
Muhammed Fatih ŞEN <76725866+mfatiihsen@users.noreply.github.com>
2025-02-21 17:29:14 +0300

    Database Ekleme işlemleri

--- AdminBlog/Controllers/HomeController.cs
+++ AdminBlog/Controllers/HomeController.cs
@@ -7,10 +7,30 @@ namespace AdminBlog.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly BlogContext _context; // Veri tabanı bağlantısı
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, BlogContext context)
     {
         _logger = logger;
+        _context = context;
+    }
+
+    // Yeni kategori Eklemek İşlemi
+    public async Task<IActionResult> addCategory(Category category)
+    {
+        await _context.AddAsync(category);  // kategoriyi context'e ekle
+        await _context.SaveChangesAsync(); // verileri veri tabanına kaydediyoruz.
+
+        return RedirectToAction(nameof(Category));
+    }
+
+    // Kategori Silme İşlemleri
+    public async Task<IActionResult> DeleteCategory(int? id)
+    {
+        Category category = await _context.Category.FindAsync(id); // id'ye sahip kategoriyi buluyoruz
+        _context.Remove(category);
+        await _context.SaveChangesAsync(); // verileri kaydediyoruz.
+        return RedirectToAction(nameof(Category)); // silme işlemi başarıyle gerçekleşirse gidilecek sayfa.
     }
 
     public IActionResult Index()
@@ -18,9 +38,10 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Category()
     {
-        return View();
+        List<Category> list = _context.Category.ToList(); // tüm kategorileri listeye çekiyoruz 
+        return View(list);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
